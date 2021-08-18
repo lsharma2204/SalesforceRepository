194ec4da2508e87f70c17dce1e93c8d1f4f9d532
Lakshaya Sharma <lakshayashrm56@gmail.com>
2021-08-18 22:45:10 +0530

    VS CODE TEST PROJECT

--- LWCPractice/LWCPracticeFolder/force-app/main/default/lwc/lwc_codebook_eg4/lwc_codebook_eg4.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement,wire } from 'lwc';
-import getContactInfo from '@salesforce/apex/CL_CodeBook_Eg4.getContactInfo'
-
-export default class Lwc_codebook_eg4 extends LightningElement {
-
-    @wire(getContactInfo) lakshaya;
-}
--- LWCPractice/LWCPracticeFolder/force-app/main/default/lwc/lwc_community_test/lwc_community_test.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement,wire,track } from 'lwc';
-import fetchResult from '@salesforce/apex/TestRichApex.fetchResult';
-
-export default class Lwc_community_test extends LightningElement {
-
-    
-    @wire(fetchResult) contacts;
-
-}
